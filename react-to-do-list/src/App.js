75afcf5dc2558d5766cf4fc8e6d4afa48c5a74c6
Marcos <mp2772@gmail.com>
2018-08-04 14:10:27 -0400

    got to do list to display

--- react-to-do-list/src/App.js
+++ react-to-do-list/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      theTasks: []
+    };
+  }
+
+  getAllTheTasks(){
+    axios.get("http://localhost:5000/api/tasks")
+    .then(allTheTasks => {
+      console.log(allTheTasks);
+      this.setState({theTasks: allTheTasks.data});
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  showTasks() {
+    return (
+      this.state.theTasks.reverse().map((task, index) => {
+        return(
+          <div key={index}>
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
+          </div>
+        )
+      })
+    )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="center" >
+        <h1> The Greatest Sing To Do List of All Time </h1>
+        <button onClick={()=>{this.getAllTheTasks()}} >This is a Button </button>
+        {this.showTasks()}
       </div>
     );
   }
