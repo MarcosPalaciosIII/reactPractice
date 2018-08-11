eec81e17a1a5eb8bfea94b3ad9f11a099315d91f
Marcos <mp2772@gmail.com>
2018-08-11 14:38:14 -0400

    set up form to appear when no user logged in, have log out working

--- react-to-do-list/src/user.js
+++ react-to-do-list/src/user.js
@@ -35,9 +35,10 @@ class User extends Component {
     const password = this.state.passwordInput;
     axios.post('http://localhost:5000/api/login', {username, password}, {withcredentials: true})
     .then((response) => {
+
       this.setState({
-        usernameInput: this.state.usernameInput,
-        passwordInput: this.state.passwordInput,
+        usernameInput: '',
+        passwordInput: '',
         loggedInUser: response.data,
       });
 
@@ -52,10 +53,29 @@ class User extends Component {
     const password = this.state.passwordInput;
     axios.post('http://localhost:5000/api/signup', {username, password}, {withcredentials: true})
     .then((response) => {
+
       this.setState({
-        usernameInput: this.state.usernameInput,
-        passwordInput: this.state.passwordInput,
+        usernameInput: '',
+        passwordInput: '',
         loggedInUser: response.data,
+      });
+
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  logout(){
+    const username = this.state.usernameInput;
+    const password = this.state.passwordInput;
+    axios.post('http://localhost:5000/api/logout', {username, password}, {withcredentials: true})
+    .then((response) => {
+
+      this.setState({
+        usernameInput: '',
+        passwordInput: '',
+        loggedInUser: null,
       });
 
     })
@@ -67,27 +87,73 @@ class User extends Component {
 
 
   showUser(){
-    return this.state.loggedInUser ? `Welcome, ${this.state.loggedInUser.username}` : "User Component";
+    this.fetchUser();
+    if(this.state.loggedInUser) {
+
+      return (
+        <div className="center">
+
+          <h3>Welcome, {this.state.loggedInUser.username}</h3>
+          <button onClick={()=>{this.logout()}} className="logOutButton"> Log Out </button>
+
+        </div>
+      );
+
+    } else {
+      return "User Component";
+    }
+  }
+
+  fetchUser(){
+    if( this.state.loggedInUser === null ) {
+      axios.get('http://localhost:5000/api/loggedin', {withcredentials: true})
+      .then((response) => {
+        this.setState({
+          usernameInput: this.state.usernameInput,
+          passwordInput: this.state.passwordInput,
+          loggedInUser: response.data,
+        });
+
+      })
+      .catch((err) => {
+        this.setState({
+          usernameInput: this.state.usernameInput,
+          passwordInput: this.state.passwordInput,
+          loggedInUser: false,
+        });
+      })
+    }
+  }
+
+  showlogInForm() {
+    if( !this.state.loggedInUser ) {
+      return(
+        <div className="add-task center">
+
+          <h3> {this.showUser()} </h3>
+
+          {/* <div> Username Here </div> */}
+
+          <labal> Username </labal>
+          <input type="text" value={this.state.usernameInput} onChange={(e)=>{this.updateUsername(e)}} />
+          <br />
+          <labal> Password </labal>
+          <input type="password" value={this.state.passwordInput} onChange={(e)=>{this.updatePassword(e)}} />
+          <br />
+          <button onClick={()=>{this.login()}} className="logIn-signUp-button"> Log In </button>
+          <button onClick={()=>{this.signup()}} className="logIn-signUp-button"> Sign Up </button>
+
+        </div>
+      )
+    }
   }
 
 
   render(){
     return(
-      <div className="add-task center">
-
-        <h3> {this.showUser()} </h3>
-
-        {/* <div> Username Here </div> */}
-
-        <labal> Username </labal>
-        <input type="text" value={this.state.usernameInput} onChange={(e)=>{this.updateUsername(e)}} />
-        <br />
-        <labal> Password </labal>
-        <input type="password" value={this.state.passwordInput} onChange={(e)=>{this.updatePassword(e)}} />
-        <br />
-        <button onClick={()=>{this.login()}} className="logIn-signUp-button"> Log In </button>
-        <button onClick={()=>{this.signup()}} className="logIn-signUp-button"> Sign Up </button>
-
+      <div>
+        {this.showlogInForm()}
+        {this.showUser()}
       </div>
     )
   }
