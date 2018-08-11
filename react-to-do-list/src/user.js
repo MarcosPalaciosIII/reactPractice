a5bc6847a7534c74cd08dc97bd6619ff0361ce1f
Marcos <mp2772@gmail.com>
2018-08-11 13:16:26 -0400

    got user login working

--- react-to-do-list/src/user.js
+++ react-to-do-list/src/user.js
@@ -7,9 +7,40 @@ class User extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      usernameInput: '',
+      passwordInput: '',
     };
   }
+
+
+  updateUsername(e){
+    this.setState({
+      usernameInput: e.target.value,
+      passwordInput: this.state.passwordInput,
+    });
+    console.log(this.state);
+  }
+
+  updatePassword(e){
+    this.setState({
+      usernameInput: this.state.usernameInput,
+      passwordInput: e.target.value,
+    });
+    console.log(this.state);
+  }
+
+  login(){
+    const username = this.state.usernameInput;
+    const password = this.state.passwordInput;
+    axios.post('http://localhost:5000/api/login', {username, password}, {withcredentials: true})
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
 
   render(){
     return(
@@ -20,9 +51,11 @@ class User extends Component {
         {/*<div> Username Here </div>*/}
 
         <labal> Username </labal>
-        <input type="text" />
+        <input type="text" value={this.state.usernameInput} onChange={(e)=>{this.updateUsername(e)}} />
         <labal> Password </labal>
-        <input type="password" />
+        <input type="password" value={this.state.passwordInput} onChange={(e)=>{this.updatePassword(e)}} />
+
+        <button onClick={()=>{this.login()}}> Log In </button>
 
       </div>
     )
